4e28f20f6f4ef0a8bbb495ceea60205c5b6c88ca
Jefwillems <jefwillems100@gmail.com>
2024-10-02 15:53:12 +0200

    test: add extra tests

--- Kroker.Stomp.Tests/FrameParserTests.cs
+++ Kroker.Stomp.Tests/FrameParserTests.cs
@@ -49,4 +49,62 @@ public class FrameParserTests
         Assert.Equal("13", result.Headers["content-length"]);
         Assert.Equal("{\"key\":\"value\"}", result.Content);
     }
+    
+    
+    [Fact]
+    public void ParsingFrameWithInvalidContentLength_ShouldReturnFalse()
+    {
+        var parser = new StompFrameParser();
+
+        var frame = new StompFrame("MESSAGE", new Dictionary<string, string>()
+        {
+            { "content-type", "application/text" },
+            { "content-length", "5" }
+        }, "Too Long Content");
+
+        _ = parser.TryWriteFrame(frame, out var buffer);
+        var x = Encoding.UTF8.GetString(buffer.ToArray());
+        var f = parser.TryParseFrame(ref buffer, out var result);
+        Assert.False(f);
+        Assert.Null(result);
+    }
+    
+    [Fact]
+    public void ParsingFrameWithEmptyContent_ShouldReturnOk()
+    {
+        var parser = new StompFrameParser();
+
+        var frame = new StompFrame("MESSAGE", new Dictionary<string, string>()
+        {
+            { "content-type", "application/text" },
+            { "content-length", "0" }
+        }, string.Empty);
+
+        _ = parser.TryWriteFrame(frame, out var buffer);
+        var x = Encoding.UTF8.GetString(buffer.ToArray());
+        var f = parser.TryParseFrame(ref buffer, out var result);
+        Assert.True(f);
+        Assert.NotNull(result);
+        Assert.Equal("MESSAGE", result.Command);
+        Assert.Equal("application/text", result.Headers["content-type"]);
+        Assert.Equal("0", result.Headers["content-length"]);
+        Assert.Equal(string.Empty, result.Content);
+    }
+    
+    [Fact]
+    public void ParsingFrameWithNoHeaders_ShouldReturnOk()
+    {
+        var parser = new StompFrameParser();
+
+        var frame = new StompFrame("MESSAGE", new Dictionary<string, string>(), "No Headers");
+
+        _ = parser.TryWriteFrame(frame, out var buffer);
+        var x = Encoding.UTF8.GetString(buffer.ToArray());
+        var f = parser.TryParseFrame(ref buffer, out var result);
+        Assert.True(f);
+        Assert.NotNull(result);
+        Assert.Equal("MESSAGE", result.Command);
+        Assert.Empty(result.Headers);
+        Assert.Equal("No Headers", result.Content);
+    }
 }
